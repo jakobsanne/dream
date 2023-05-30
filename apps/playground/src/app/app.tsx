912915526d732bec2af96150c45e09384aaace02
Jakob Sanne <jakob.sanne@gmail.com>
2023-05-30 09:19:20 +0200

    add basic select from radix lib

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -1,12 +1,25 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
-import {Select} from "@dream/ui";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@dream/ui';
 
 export function App() {
   return (
-    <div>
-      Playground app, anything goes!
-      <Select/>
+    <div className={styles['container']}>
+      <h3>Playground app</h3>
+      <div>
+        <Select onValueChange={(val) => console.log(val)} onOpenChange={(val) => console.log(val)}>
+          <SelectTrigger>
+            <SelectValue placeholder="Säg ett datum" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="1">Ett</SelectItem>
+            <SelectItem value="2">Två</SelectItem>
+            <SelectItem value="3">Tre</SelectItem>
+            <SelectItem value="4">Fyra</SelectItem>
+          </SelectContent>
+        </Select>
+        ⬅ This is a Select component
+      </div>
+
     </div>
   );
 }
--- packages/ui/src/lib/select/select.tsx
+++ packages/ui/src/lib/select/select.tsx
@@ -1,14 +1,53 @@
-import styles from './select.module.scss';
+/*
+* https://www.radix-ui.com/docs/primitives/components/select
+* */
 
-/* eslint-disable-next-line */
-export interface SelectProps {}
+import React from 'react';
+import * as SelectPrimitive from '@radix-ui/react-select';
+import './select.styles.css';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
 
-export function Select(props: SelectProps) {
-  return (
-    <div className={styles['container']}>
-      <h1>[this could be a select or other component]</h1>
-    </div>
-  );
-}
+const Select = SelectPrimitive.Root
 
-export default Select;
+const SelectGroup = SelectPrimitive.Group // implement ?
+
+const SelectValue = SelectPrimitive.Value;
+
+const SelectItem = React.forwardRef<
+  React.ElementRef<typeof SelectPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Item>
+>(({ className, children, ...props }, ref) => (
+  <SelectPrimitive.Item
+    ref={ref}
+    className={'SelectItem'}
+    {...props}
+  >
+    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+  </SelectPrimitive.Item>
+))
+
+const SelectTrigger = React.forwardRef<
+  React.ElementRef<typeof SelectPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
+>(({children, ...props}, ref) => (
+  <SelectPrimitive.Trigger className={'SelectTrigger'} ref={ref} {...props}>
+    {children}
+    <SelectPrimitive.Icon>
+      <ChevronDownIcon />
+    </SelectPrimitive.Icon>
+  </SelectPrimitive.Trigger>
+));
+
+const SelectContent = React.forwardRef<
+  React.ElementRef<typeof SelectPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
+>(({children,...props}, ref) => (
+    <SelectPrimitive.Content className={'SelectContent'} ref={ref} {...props}>
+      <SelectPrimitive.Viewport className={'SelectViewport'}>{children}</SelectPrimitive.Viewport>
+    </SelectPrimitive.Content>
+));
+
+
+export { Select, SelectGroup, SelectTrigger, SelectContent, SelectValue, SelectItem };
+
+
